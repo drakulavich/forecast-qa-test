142bd4bbeefabc48dc9ffc01adc53128a40b5b50
Anton Yakutovich <drakulavich@gmail.com>
2021-04-05 00:26:31 +0400

    Use different exit codes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,24 +2,28 @@ import api.ForecastServiceImpl;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.io.IOException;
 import java.time.LocalDate;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            System.out.println("Pass city name as an argument");
+    public static void main(String[] args) {
+        try {
+            if (args.length < 1) {
+                throw new IllegalArgumentException("Pass city name as an argument");
+            }
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://www.metaweather.com")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            LocalDate tomorrow = LocalDate.now().plusDays(1);
+
+            ForecastServiceImpl service = new ForecastServiceImpl(retrofit);
+            System.out.println(service.getForecast(args[0], tomorrow));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
             System.exit(1);
         }
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.metaweather.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        LocalDate tomorrow = LocalDate.now().plusDays(1);
-
-        ForecastServiceImpl service = new ForecastServiceImpl(retrofit);
-        System.out.println(service.getForecast(args[0], tomorrow));
+        System.exit(0);
     }
 }
